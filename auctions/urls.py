032f1f045bc49c637260fdabc57654c333ab1b6f
yogeshsingh <yogeshsingh101200@gmail.com>
2020-08-07 14:10:33 +0530

    added register, login and logout

--- auctions/urls.py
+++ auctions/urls.py
@@ -1,2 +1,13 @@
 """ Maps views to urls """
-urlpatterns = []
+from django.urls import path
+
+from . import views
+
+app_name = "auctions"
+
+urlpatterns = [
+    path("", views.index, name="index"),
+    path("register/", views.register, name="register"),
+    path("login/", views.login_view, name="login"),
+    path("logout/", views.logout_view, name="logout")
+]
--- auctions/views.py
+++ auctions/views.py
@@ -1,3 +1,54 @@
-from django.shortcuts import render
+""" Views """
 
-# Create your views here.
+from django.http import HttpResponseRedirect
+from django.urls import reverse
+from django.db import IntegrityError
+from django.shortcuts import render
+from django.contrib.auth.models import User
+from django.contrib.auth import login, logout, authenticate
+from django.contrib.auth.decorators import login_required
+
+
+@login_required
+def index(request):
+    """ main route """
+    return render(request, "auctions/index.html")
+
+
+def register(request):
+    """ Register a user to website """
+    if request.method == "POST":
+        username = request.POST["username"]
+        password = request.POST["password"]
+        confirmation = request.POST["confirmation"]
+        if password != confirmation:
+            return render(request, "auctions/resgister.html")
+        try:
+            user = User.objects.create_user(username, password=password)
+        except IntegrityError:
+            return render(request, "auctions/register.html", {
+                "message": "Invalid credentials!"
+            })
+        login(request, user)
+        return HttpResponseRedirect(reverse("auctions:index"))
+    return render(request, "auctions/register.html")
+
+
+def login_view(request):
+    """ Logout a user """
+    if request.method == "POST":
+        username = request.POST["username"]
+        password = request.POST["password"]
+        user = authenticate(request, username=username, password=password)
+        if user is not None:
+            login(request, user)
+            if request.GET.get("next"):
+                return HttpResponseRedirect(request.GET["next"])
+            return HttpResponseRedirect(reverse("auctions:index"))
+    return render(request, "auctions/login.html")
+
+
+def logout_view(request):
+    """ Logout a user """
+    logout(request)
+    return HttpResponseRedirect(reverse('auctions:login'))
