be2dd4babd1a67a3b3cca0deb1718e2f2c08587a
yogeshsingh <yogeshsingh101200@gmail.com>
2020-08-28 20:40:31 +0530

    created model form to register user

---
+++ auctions/forms.py
@@ -0,0 +1,11 @@
+""" Forms """
+
+from django.contrib.auth.forms import UserCreationForm
+from .models import User
+
+
+class RegisterForm(UserCreationForm):
+    """ Form to register user """
+    class Meta(UserCreationForm.Meta):
+        model = User
+        fields = UserCreationForm.Meta.fields
