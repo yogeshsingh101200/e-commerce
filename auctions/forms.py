3ecdaee738c36ce16983dd57e7aec15bd4f8a6d2
yogeshsingh <yogeshsingh101200@gmail.com>
2020-08-30 19:17:15 +0530

    added model form for Bid

--- auctions/forms.py
+++ auctions/forms.py
@@ -1,10 +1,18 @@
 """ Forms """
 
+from django import forms
 from django.contrib.auth.forms import UserCreationForm
-from .models import User
+from .models import User, Bid
 
 
 class RegisterForm(UserCreationForm):
     """ Form to register user """
     class Meta(UserCreationForm.Meta):
         model = User
+
+
+class BidForm(forms.ModelForm):
+    """ maps to bid model """
+    class Meta:
+        model = Bid
+        fields = "__all__"
