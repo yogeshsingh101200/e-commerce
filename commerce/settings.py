1703c98ae7feb7ae75405292546b9f4d7e5b700e
yogeshsingh <yogeshsingh101200@gmail.com>
2020-09-06 15:19:13 +0530

    prepared for deployment to heroku

--- commerce/settings.py
+++ commerce/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/3.0/ref/settings/
 """
 
 import os
+import django_heroku
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -124,3 +125,6 @@ STATIC_URL = '/static/'
 
 
 LOGIN_URL = "auctions:login"
+
+# Activate Django-Heroku.
+django_heroku.settings(locals())
